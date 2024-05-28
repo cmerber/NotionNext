43a676149117bf2053ebdaf82d4425689ec113ab
cmerber <145759984+cmerber@users.noreply.github.com>
2024-05-28 11:24:38 +0800

    Update GlobalHead.js

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -31,14 +31,7 @@ const GlobalHead = props => {
       <meta name='theme-color' content={siteConfig('BACKGROUND_DARK')} />
       <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0' />
       <meta name='robots' content='follow, index' />
-      <meta charSet='UTF-8' />
-      <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "mitvq51ouk");
-      </script>                                             
+      <meta charSet='UTF-8' />                                            
       {siteConfig('SEO_GOOGLE_SITE_VERIFICATION') && (
         <meta name='google-site-verification' content={siteConfig('SEO_GOOGLE_SITE_VERIFICATION')} />
       )}
