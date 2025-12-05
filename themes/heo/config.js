bbe7eba89fa61d7db86b9978c7b173c0d0131d25
drqc <145759984+cmerber@users.noreply.github.com>
2025-12-05 10:27:12 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://qc914.org' },
-    { title: '访问文档中心获取更多帮助', url: 'https://qc914.org' }
+    { title: '欢迎来到我的博客', url: [] },
+    { title: '访问文档中心获取更多帮助', url: [] }
   ],
 
   // 英雄区左右侧组件颠倒位置
